875ccadcd2ee9bc16aa509ede4d0635aa17f9f84
Reduc0r <timurhay1@gmail.com>
2026-03-11 21:53:35 +0300

    refactor delete unused

--- app/src/main/java/com/example/noteslist/presentation/MainActivity.kt
+++ app/src/main/java/com/example/noteslist/presentation/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.example.noteslist.presentation
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.noteslist.data.repository.NoteRepositoryImpl
