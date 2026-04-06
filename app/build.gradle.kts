81c64b0949bd38db57363aaf1888f6e3742a701d
Reduc0r <timurhay1@gmail.com>
2026-04-06 19:09:51 +0300

    add fragment layout

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,4 +50,5 @@ dependencies {
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.recyclerview)
+    implementation(libs.androidx.fragment.ktx)
 }
